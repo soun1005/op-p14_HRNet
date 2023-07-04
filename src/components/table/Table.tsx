f9da32135ef5f283b31ceca5b85a19ec687ef516
Soeun Lee <thdms1005@gmail.com>
2023-07-04 15:43:41 +0200

    table sorting

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,74 +1,137 @@
+import React, { useState } from 'react';
 import {
+  ColumnDef,
+  // ColumnDef,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from '@tanstack/react-table';
-import React from 'react';
-import defaultData from '../../data/mockEmployees';
-import { columns } from './columns';
+import { makeData } from './makeData';
+import FormValues from '../../type/formType';
+import mockData from '../../data/mockEmployees';
+// import columns from './columns';
 
-export const Table = () => {
+export function Table() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  // columns
+  const columns = React.useMemo<ColumnDef<FormValues>[]>(
+    () => [
+      {
+        header: () => <span>First Name</span>,
+        accessorKey: 'firstName',
+      },
+      {
+        accessorKey: 'lastName',
+        header: () => <span>Last Name</span>,
+      },
+      {
+        accessorKey: 'startDate',
+        header: () => <span>Start Date</span>,
+      },
+      {
+        accessorKey: 'department',
+        header: () => <span>Department</span>,
+      },
+      {
+        accessorKey: 'birthDate',
+        header: () => <span>Date of Birth</span>,
+      },
+      {
+        accessorKey: 'street',
+        header: () => <span>Street</span>,
+      },
+      {
+        accessorKey: 'city',
+        header: () => <span>City</span>,
+      },
+      {
+        accessorKey: 'state',
+        header: () => <span>State</span>,
+      },
+      {
+        accessorKey: 'zip',
+        header: () => <span>Zip Code</span>,
+      },
+    ],
+    []
+  );
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [data, setData] = React.useState(() => [...defaultData]);
-  //   const rerender = React.useReducer(() => ({}), {})[1];
+  const [data, setData] = useState<FormValues[]>(() => makeData(mockData));
 
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
   });
 
   return (
     <div className="p-2">
+      <div className="h-2" />
       <table>
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: ' 🔼',
+                          desc: ' 🔽',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </th>
+                );
+              })}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {table
+            .getRowModel()
+            .rows.slice(0, 10)
+            .map((row) => {
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
         </tbody>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
       </table>
-      <div className="h-4" />
-      {/* <button onClick={() => rerender()} className="border p-2">
-        Rerender
-      </button> */}
+      <div> Total : {table.getRowModel().rows.length} Employees</div>
     </div>
   );
-};
+}
---
+++ src/components/table/makeData.tsx
@@ -0,0 +1,5 @@
+import FormValues from '../../type/formType';
+
+export function makeData(data: FormValues[]): FormValues[] {
+  return data;
+}
